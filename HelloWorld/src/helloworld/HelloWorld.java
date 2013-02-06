975a356d05965c77d154d82ec149c100c3c7ba09
Your Name <you@example.com>
2013-02-06 16:51:01 -0500

    Added HelloWorld

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+/**
+ *
+ * @author Devon Dimone 106 980 158
+ */
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("HelloWorld!");
+    }
+}
