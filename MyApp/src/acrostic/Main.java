8cce10f1bf2cc7980d98ea18731c89497f19ed2c
Devon Dimone <>
2013-02-06 22:10:36 -0500

    Initialized and Finished Step 3 (NetBeans Tutorial)

---
+++ MyApp/src/acrostic/Main.java
@@ -0,0 +1,20 @@
+package acrostic;
+
+import org.me.mylib.LibClass;
+
+/**
+ * Project for class
+ * @author Devon Dimone 106 980 158
+ */
+public class Main 
+{
+    /**
+     * something
+     * @param args something
+     */
+    public static void main(String[] args) 
+    {
+        String result = LibClass.acrostic(args);
+        System.out.println("Result = " + result);
+    }
+}
