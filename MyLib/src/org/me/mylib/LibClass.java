cb32dbef2b74313da14ed1d5d4fc9bef75a4b570
Your Name <you@example.com>
2013-02-07 10:45:21 -0500

    Initialized Anagram Game

---
+++ MyLib/src/org/me/mylib/LibClass.java
@@ -0,0 +1,30 @@
+package org.me.mylib;
+
+/**
+ *
+ * @author Devon Dimone
+ */
+public class LibClass 
+{
+    /**
+     * 
+     * @param args
+     * @return 
+     */
+    public static String acrostic(String[] args) 
+    {
+        StringBuffer b = new StringBuffer();
+        for (int i = 0; i < args.length; i++) 
+        {
+            if (args[i].length() > i) 
+            {
+                b.append(args[i].charAt(i));
+            } 
+            else 
+            {
+                b.append('?');
+            }
+        }
+        return b.toString();
+    }    
+}
